319aa254477ddbadecfebb3dc41eea9443b1615f
aldo <aldooofox@gmail.com>
2026-06-12 21:43:18 +0700

    Menambahkan state untuk data trip dan filter wisata

--- user/src/app/trip/index.tsx
+++ user/src/app/trip/index.tsx
@@ -1,6 +1,13 @@
 import { View, Text } from "react-native";
+import { useState } from "react";
 
 export default function TripScreen() {
+  const [wisata, setWisata] = useState<Wisata[]>([]);
+  const [selectedWisataId, setSelectedWisataId] = useState<number | "all">("all");
+  const [data, setData] = useState<Trip[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
   return (
     <View>
       <Text>Halaman Trip</Text>
