91fd7d0b9f8047ed93a05c5ef05a31665372087a
hikmal <muhammad_hikmal_ramadhan@teknokrat.ac.id>
2026-06-13 00:12:59 +0700

    Menambahkan state data booking

--- user/src/app/my-bookings/index.tsx
+++ user/src/app/my-bookings/index.tsx
@@ -1,6 +1,12 @@
 import { View, Text } from "react-native";
+import { useState } from "react";
 
 export default function MyBookingsScreen() {
+  const [data, setData] = useState<Booking[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [updatingId, setUpdatingId] = useState<number | null>(null);
+  
   return (
     <View>
       <Text>My Bookings</Text>
