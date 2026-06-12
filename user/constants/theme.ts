916e4c13046c683ac52dec09bfa684d05b74eb76
aldo <aldooofox@gmail.com>
2026-06-12 21:15:29 +0700

    theme tampilan

---
+++ user/constants/theme.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  light: {
+    background: '#ffffff',
+    backgroundElement: '#0f172a',
+    text: '#0f172a',
+  },
+  dark: {
+    background: '#0b1220',
+    backgroundElement: '#334155',
+    text: '#ffffff',
+  },
+} as const;
+
