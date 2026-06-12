a6ffe2d75b1dd8e2a8242526fe991a674f1cccd8
hikmal <muhammad_hikmal_ramadhan@teknokrat.ac.id>
2026-06-12 22:50:32 +0700

    Menambahkan integrasi API booking

--- user/src/app/booking/index.tsx
+++ user/src/app/booking/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from "react-native";
 import { useState } from "react";
+import { apiTrip, apiWisata, apiBooking, type Trip, type Wisata } from "../../../lib/api";
 
 export default function BookingScreen() {
   return (
