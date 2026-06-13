b5a58d44f5ae4dff18e26ce6e2a910d24a26acf5
aldo <aldooofox@gmail.com>
2026-06-13 17:32:04 +0700

    Menambahkan service wisata trip dan booking pada gateway

--- api/src/gateway/gateway.controller.ts
+++ api/src/gateway/gateway.controller.ts
@@ -2,5 +2,15 @@ import {
   Controller,
 } from '@nestjs/common';
 
+import { WisataService } from '../wisata/wisata.service';
+import { TripService } from '../trip/trip.service';
+import { BookingService } from '../booking/booking.service';
+
 @Controller('api')
-export class GatewayController {}
+export class GatewayController {
+  constructor(
+    private readonly wisataService: WisataService,
+    private readonly tripService: TripService,
+    private readonly bookingService: BookingService,
+  ) {}
+}
