44d8bff3a3581129acb19df7ec4c668655b18a5a
aldo <aldooofox@gmail.com>
2026-06-13 17:33:26 +0700

    Menambahkan endpoint CRUD wisata pada gateway

--- api/src/gateway/gateway.controller.ts
+++ api/src/gateway/gateway.controller.ts
@@ -5,6 +5,8 @@ import {
 import { WisataService } from '../wisata/wisata.service';
 import { TripService } from '../trip/trip.service';
 import { BookingService } from '../booking/booking.service';
+import { CreateWisataDto } from '../wisata/dto/create-wisata.dto';
+import { UpdateWisataDto } from '../wisata/dto/update-wisata.dto';
 
 @Controller('api')
 export class GatewayController {
@@ -13,4 +15,14 @@ export class GatewayController {
     private readonly tripService: TripService,
     private readonly bookingService: BookingService,
   ) {}
+
+//Wisata
+  @Post('wisata')
+  @Get('wisata')
+  @Get('wisata/:id')
+  @Patch('wisata/:id')
+  @Delete('wisata/:id')
+
 }
+
+
