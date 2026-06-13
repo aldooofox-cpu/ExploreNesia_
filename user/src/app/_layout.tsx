2eb102ffc23bce85415b0f53b4f2decb5c688767
aldo <aldooofox@gmail.com>
2026-06-13 16:24:02 +0700

    Memperbaiki Headbar

--- user/src/app/_layout.tsx
+++ user/src/app/_layout.tsx
@@ -1,6 +1,18 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "" }} />
+      <Stack.Screen name="wisata" options={{ title: "Wisata" }} />
+      <Stack.Screen name="trip" options={{ title: "Trip" }} />
+      <Stack.Screen name="booking" options={{ title: "Booking" }} />
+      <Stack.Screen name="my-bookings" options={{ title: "My Bookings" }} />
+    </Stack>
+  );
 }
-
